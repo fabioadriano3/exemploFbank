8c5c5d16d2763d6ebf7f9e2043212fa1d133a942
fabio silva <resource.frodrigues@bv.com.br>
2025-02-20 14:07:26 -0300

    Criação dos cenarios de validar cpf

--- PLATAFORMAS/MOBILE/Screens/criar_conta.py
+++ PLATAFORMAS/MOBILE/Screens/criar_conta.py
@@ -4,9 +4,13 @@
  
 
 #*** Variables ***
-
-#${btn_acessar_conta}  Set Variables "//*[contains(@text,'Acesse sua conta')]"
-#${btn_acessar_conta}=   Set Variable  //android.widget.TextView[@text='Acesse sua conta']
-#${btn_acessar_conta}=    Click Element                          //android.widget.TextView[@text='Acesse sua conta']  
 btn_Criar_conta = "//android.widget.TextView[@text='Criar uma conta']" 
 
+termoAceite = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup"
+validartextoTermos="//android.widget.TextView[@text='Antes de avançar, leia e aceite os termos de abertura de conta, termos de uso e políticas de privacidade do Potência.'']"
+botaoOk= "//android.widget.TextView[@text='Ok, entendi']"
+botaoLocalizacao = "//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_foreground_only_button']"
+validarTextQualCpf = "//android.widget.TextView[@text='Diz pra gente qual é seu CPF']"
+dgtCpf = "//android.widget.EditText[@text='Número do CPF']"
+botaoAvancar="//android.widget.TextView[@text='Avançar']" 
+valiadarTextoCpfIncorrereto="//android.widget.TextView[@text='Este CPF está incorreto ou não existe']"
--- PLATAFORMAS/MOBILE/feature/criar_conta.robot
+++ PLATAFORMAS/MOBILE/feature/criar_conta.robot
@@ -1,5 +1,6 @@
 *** Settings ***
 Library        AppiumLibrary
+#Library    ScreenCapLibrary
 
 Resource        ../../MOBILE/Resources/startconnect.robot
 Resource        ../../MOBILE/Steps/criar_conta.robot
@@ -11,10 +12,26 @@ Test Teardown   close session
 *** Test Cases ***
 
 
-Validar cpf invalido 
+Validar cpf valido 
+ 
   clicar no botao criar_conta 
-
-#Validar cpf valido 
- # clicar no botao acessar_conta
+  Clicar e aceitar o termo de aceite 
+  Clicar no botao ok de termos de aceite
+  Clicar no botao de permisao de localizaçao
+  Sleep    5
+  Validar texto qual seu cpf 
+  Clicar no campo e Digitar Cpf Valido
+  Clicar no avancar 
+  
+Validar cpf invalido 
+ 
+  clicar no botao criar_conta 
+  Clicar e aceitar o termo de aceite 
+  Clicar no botao ok de termos de aceite
+  Clicar no botao de permisao de localizaçao
+  Sleep    5
+  Validar texto qual seu cpf 
+  Clicar no campo e Digitar Cpf invalido
+  Validar texto de cpf invalido
 
 
